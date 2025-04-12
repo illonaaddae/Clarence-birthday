e576584effff7eec6ee94046cd6a3285383edebe
Illona Addae <122870725+illonaaddae@users.noreply.github.com>
2025-04-12 23:47:26 +0000

    edited input button

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var text =
+let text =
   " Today, I celebrate not just your birth, but your incredible presence in my life and in the journey of SLINT Tech. You’ve been more than a friend, you’re a true co-founder, a pillar of strength, and a visionary partner. Keep being the light and the leader you are we have a future to build together.";
-var word = text.split("");
-var i = 0;
-var interval = setInterval(writeText, 80);
+let word = text.split("");
+let i = 0;
+let interval = setInterval(writeText, 80);
 function writeText() {
-  var p = document.getElementsByTagName("p")[0];
+  let p = document.getElementsByTagName("p")[0];
   if (i < word.length) {
     p.innerHTML += word[i];
     i++;
@@ -16,7 +16,7 @@ function Z() {
   swal(
     "You're the unit of sweetness kitty🍫🍫🍫🍰🍰🍰🍬🍬🍬🍨🍨🍧🍧🍧🍦🍦🍦🍦🍦🍦!"
   );
-  var x = document.getElementById("Z");
+  let x = document.getElementById("Z");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
@@ -26,7 +26,7 @@ function Z() {
 
 function X() {
   swal("Youre cutest kitty!🌹🌹🌹🌹🌸🌸🌸🌼🌼🌼🌻🌻🌻🌺🌺🌺");
-  var x = document.getElementById("X");
+  let x = document.getElementById("X");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
@@ -36,7 +36,7 @@ function X() {
 
 function Y() {
   swal("Meowwwwwwwwww😂😂😂😹😹😹😹 You're always Gooooooood 😺 like a 🐈😻😽");
-  var x = document.getElementById("Y");
+  let x = document.getElementById("Y");
   if (x.style.display === " ") {
     x.style.display = "block";
   } else {
